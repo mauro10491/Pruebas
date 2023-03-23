66de73cfc1e86a1216ef44fb7ae21846556aff1f
mauro10491 <mauricio.alzate13706@udecaldas.edu.co>
2023-03-23 14:02:07 -0500

    Agrgando prueba

---
+++ PruebasGit/src/main/java/poo/pruebasgit/PruebasGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pruebasgit;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PruebasGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
